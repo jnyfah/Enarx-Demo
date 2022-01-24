cb11381603997925a4414f540f5869578087c3ce
jnyfah <jnyfaah@gmail.com>
2022-01-24 15:18:25 +0100

    prediction accuracy

--- src/main.rs
+++ src/main.rs
@@ -50,6 +50,9 @@ impl BostonDataSet {
 fn main() -> Result<(), Box<dyn Error>> {
 
     let args: Vec<String> = env::args().collect();
+
+
+    println!("external file path {:?}", args[1]);
    
 
     let file_path = &args[1];
@@ -109,6 +112,7 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     //println!("yhat train shape: {:?}", yhat_train.shape());
     //println!("yhat test shape: {:?}", yhat_test.shape());
+    
     let mae_train = (&yhat_train - &train_dataset.labels)
         .map(|y| y.abs())
         .mean()
@@ -118,8 +122,19 @@ fn main() -> Result<(), Box<dyn Error>> {
         .mean()
         .unwrap();
 
-    println!("Tree size: {}", &decision_tree.size());
-    println!("MAE Train: {:?}", mae_train);
-    println!("MAE Test: {:?}", mae_test);
+    println!("");
+
+    let _Train_Accuracy = (1.0-mae_train)*100.0;
+    let _Test_Accuracy = (1.0-mae_test)*100.0;
+
+    //println!("Tree size: {}", &decision_tree.size());
+
+    println!("Train Accuracy: {:?} %", _Train_Accuracy);
+
+    println!("Test Accuracy: {:?} %", _Test_Accuracy);
+
+    //println!("MAE Train: {:?}", mae_train);
+    //println!("MAE Test: {:?}", mae_test);
+
     Ok(())
 }
