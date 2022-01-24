b413ac188996ad807e84170c72eec20c0159d0d7
jnyfah <jnyfaah@gmail.com>
2022-01-24 14:44:34 +0100

    using decision trees

--- src/main.rs
+++ src/main.rs
@@ -1,67 +1,125 @@
 #![allow(non_snake_case)]
-use rml::knn;
-use std::env;
-use rml::math;
+use ndarray::{Array1, Array2, Axis, Slice};
+use serde::Deserialize;
 use std::error::Error;
-use std::time::Instant;
+use std::fs::File;
+use std::time::SystemTime;
+use std::env;
+
+use Enarx_Demo::Tree;
 
 
-type CSVOutput = (Vec<Vec<f64>>, Vec<i32>);
-
-fn parse_csv(data: &str) -> Result<CSVOutput, Box<dyn Error>> {
-    let mut out_data: CSVOutput = (Vec::new(), Vec::new());
-    let mut reader = csv::ReaderBuilder::new()
-        .has_headers(true)
-        .from_path(data)?;
-
-    for line in reader.records() {
-        let result = line?;
-        let mut line_data: (Vec<f64>, i32) = (Vec::new(), 0);
-        line_data.1 = (result.get(result.len() - 1).unwrap()).parse()?;
-        for i in 0..result.len() - 1 {
-            line_data.0.push((result.get(i).unwrap()).parse()?);
-        }
-
-        out_data.0.push(line_data.0);
-        out_data.1.push(line_data.1);
-    }
-    Ok(out_data)
+#[derive(Debug, Deserialize)]
+struct BostonRecord {
+    Pregnancies: f64,
+    Glucose: f64,
+    BloodPressure: f64,
+    SkinThickness: f64,
+    Insulin: f64,
+    Bmi: f64,
+    DiabetesPedigreeFunction: f64,
+    Age: f64,
+    Outcome: f64,
 }
 
+struct BostonDataSet {
+    features: Array2<f64>,
+    labels: Array1<f64>,
+}
+
+impl BostonDataSet {
+    pub fn from(records: Vec<BostonRecord>) -> Self {
+        let mut features = Vec::with_capacity(records.len());
+        let mut labels = Vec::with_capacity(records.len());
+
+        for r in records {
+            let feats = &[
+                r.Pregnancies, r.Glucose, r.BloodPressure, r.SkinThickness, r.Insulin, r.Bmi, r.DiabetesPedigreeFunction, r.Age,
+            ];
+            features.push(feats.clone());
+            labels.push(r.Outcome);
+        }
+
+        Self {
+            features: Array2::from(features),
+            labels: Array1::from(labels),
+        }
+    }
+}
 
 fn main() -> Result<(), Box<dyn Error>> {
+
     let args: Vec<String> = env::args().collect();
-    println!("{}", args[2]);
-    println!("{}", args[1]);
+   
 
-    // Format: (Vectors of each feature, Vector of class label)
-    let training_data = parse_csv(&args[1])?;
-    let testing_data = parse_csv(&args[2])?;
+    let file_path = &args[1];
+    let file = File::open(file_path)?;
+    let mut rdr = csv::Reader::from_reader(file);
+    let mut bfr = Vec::new();
 
-    let start = Instant::now();
+    for result in rdr.deserialize() {
+        let record: BostonRecord = result?;
+        bfr.push(record);
+    }
 
-    let knn = knn::KNN::new(
+    let dataset = BostonDataSet::from(bfr);
+    println!("Dataset size: {}", &dataset.features.nrows());
+
+    let train_dataset = BostonDataSet {
+        features: dataset
+            .features
+            .slice_axis(Axis(0), Slice::from(0..450))
+            .to_owned(),
+        labels: dataset
+            .labels
+            .slice_axis(Axis(0), Slice::from(0..450))
+            .to_owned(),
+    };
+
+    let test_dataset = BostonDataSet {
+        features: dataset
+            .features
+            .slice_axis(Axis(0), Slice::from(450..))
+            .to_owned(),
+        labels: dataset
+            .labels
+            .slice_axis(Axis(0), Slice::from(450..))
+            .to_owned(),
+    };
+
+    let start_training = SystemTime::now();
+    let decision_tree = Tree::new(
+        train_dataset.features.clone(),
+        train_dataset.labels.clone(),
         5,
-        training_data.0,
-        training_data.1,
-        None,
-        Some(math::norm::Norm::L2),
+        7,
     );
+    let end_training = start_training.elapsed().unwrap().as_millis();
+    println!("Training took {} ms", end_training);
 
-    let pred: Vec<i32> = testing_data.0.iter().map(|x| knn.predict(x)).collect();
+    let start_prediction = SystemTime::now();
+    let yhat_train = train_dataset
+        .features
+        .map_axis(Axis(1), |x| decision_tree.predict(x.to_owned()));
+    let yhat_test = test_dataset
+        .features
+        .map_axis(Axis(1), |x| decision_tree.predict(x.to_owned()));
+    let prediction_duration = start_prediction.elapsed().unwrap().as_millis();
+    println!("Prediction took {} ms", prediction_duration);
 
-    let num_correct = pred
-        .iter()
-        .cloned()
-        .zip(&testing_data.1)
-        .filter(|(a, b)| *a == **b)
-        .count();
+    //println!("yhat train shape: {:?}", yhat_train.shape());
+    //println!("yhat test shape: {:?}", yhat_test.shape());
+    let mae_train = (&yhat_train - &train_dataset.labels)
+        .map(|y| y.abs())
+        .mean()
+        .unwrap();
+    let mae_test = (&yhat_test - &test_dataset.labels)
+        .map(|y| y.abs())
+        .mean()
+        .unwrap();
 
-    println!(
-        "Accuracy: {} Runtime: {}s",
-        (num_correct as f64) / (pred.len() as f64),
-        start.elapsed().as_secs_f64()
-    );
-
+    println!("Tree size: {}", &decision_tree.size());
+    println!("MAE Train: {:?}", mae_train);
+    println!("MAE Test: {:?}", mae_test);
     Ok(())
 }
